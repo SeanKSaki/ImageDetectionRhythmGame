aa5b0b90ae4d76bb8ee2aee4fa3f2fbd81f9ce21
Eric Wu <ericwu2266@gmail.com>
2023-04-12 08:34:18 -0700

    new detection model

---
+++ AiVirtualMouse/AiVirtualMouse.py
@@ -0,0 +1,73 @@
+import cv2
+import HandTrackingModule as htm
+import autopy
+import numpy as np
+import time
+import cvzone
+
+##############################
+wCam, hCam = 1280, 720
+frameR = 144
+smoothening = 1
+##############################
+cap = cv2.VideoCapture(1)  # 若使用笔记本自带摄像头则编号为0  若使用外接摄像头 则更改为1或其他编号
+cap.set(3, wCam)
+cap.set(4, hCam)
+pTime = 0
+plocX, plocY = 0, 0
+clocX, clocY = 0, 0
+
+detector = htm.handDetector()
+wScr, hScr = autopy.screen.size()
+# print(wScr, hScr)
+
+while True:
+    success, img = cap.read()
+    # 1. 检测手部 得到手指关键点坐标
+    img = detector.findHands(img)
+    cv2.rectangle(img, (frameR, frameR), (wCam - frameR, hCam - frameR), (0, 255, 0), 2,  cv2.FONT_HERSHEY_PLAIN)
+    lmList = detector.findPosition(img, draw=False)
+
+    # 2. 判断食指和中指是否伸出
+    if len(lmList) != 0:
+        x1, y1 = lmList[20][1:]
+        fingers = detector.fingersUp()
+
+        # 3. 若只有食指伸出 则进入移动模式
+        if fingers[0] and fingers[1] and fingers[2] and fingers[3] and fingers[4] == True:
+            # 4. 坐标转换： 将食指在窗口坐标转换为鼠标在桌面的坐标
+            # 鼠标坐标
+            x3 = np.interp(x1, (frameR, wCam - frameR), (0, wScr))
+            y3 = np.interp(y1, (frameR, hCam - frameR), (0, hScr))
+            print(x3, y3)
+            # smoothening values
+            clocX = plocX + (x3 - plocX) / smoothening
+            clocY = plocY + (y3 - plocY) / smoothening
+            if (0 < wScr - clocX < 1920) and (0 < clocY < 1080):
+                print(wScr - clocX, clocY)
+                autopy.mouse.move(wScr - clocX, clocY)
+                cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
+                plocX, plocY = clocX, clocY
+
+        # 5. 若是食指和中指都伸出 则检测指头距离 距离够短则对应鼠标点击'NoneType' object has no attribute 'Label'
+        if fingers[0] and fingers[1] and fingers[2] and fingers[3] and fingers[4] == True:
+            length, img, pointInfo = detector.findDistance(8, 12, img)
+            if length < 40:
+                cv2.circle(img, (pointInfo[4], pointInfo[5]),
+                           15, (0, 255, 0), cv2.FILLED)
+                autopy.mouse.click()
+
+        if fingers[0] and fingers[1] and fingers[2] and fingers[3] and fingers[4] == True:
+            length, img, pointInfo = detector.findDistance(12, 16, img)
+            if length < 40:
+                cv2.circle(img, (pointInfo[4], pointInfo[5]),
+                           15, (0, 255, 0), cv2.FILLED)
+                autopy.mouse.click(autopy.mouse.Button.RIGHT)
+
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+    cv2.putText(img, f'fps:{int(fps)}', [15, 25],
+                cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 255), 2)
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
---
+++ AiVirtualMouse/HandTrackingModule.py
@@ -0,0 +1,100 @@
+import cv2
+import mediapipe as mp
+import time
+import math
+
+class handDetector():
+    def __init__(self, mode=False, maxHands=2, detectionCon=0.5, trackCon=0.5):
+        self.mode = mode
+        self.maxHands = maxHands
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands, 1, self.detectionCon, self.trackCon)
+        self.mpDraw = mp.solutions.drawing_utils
+        self.tipIds = [4, 8, 12, 16, 20]
+
+    def findHands(self, img, draw=True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB)
+
+        print(self.results.multi_handedness)  # 获取检测结果中的左右手标签并打印
+
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
+        return img
+
+
+    def findPosition(self, img, draw=True):
+        self.lmList = []
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
+                for id, lm in enumerate(handLms.landmark):
+                    h, w, c = img.shape
+                    cx, cy = int(lm.x * w), int(lm.y * h)
+                    # print(id, cx, cy)
+                    self.lmList.append([id, cx, cy])
+                    if draw:
+                        cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+        return self.lmList
+
+    def fingersUp(self):
+        fingers = []
+        # 大拇指
+        if self.lmList[self.tipIds[0]][1] > self.lmList[self.tipIds[0] - 1][1]:
+            fingers.append(1)
+        else:
+            fingers.append(0)
+
+        # 其余手指
+        for id in range(1, 5):
+            if self.lmList[self.tipIds[id]][2] < self.lmList[self.tipIds[id] - 2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+
+        # totalFingers = fingers.count(1)
+        return fingers
+
+    def findDistance(self, p1, p2, img, draw=True, r=15, t=3):
+        x1, y1 = self.lmList[p1][1:]
+        x2, y2 = self.lmList[p2][1:]
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+
+        if draw:
+            cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), t)
+            cv2.circle(img, (x1, y1), r, (255, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x2, y2), r, (255, 0, 255), cv2.FILLED)
+            cv2.circle(img, (cx, cy), r, (0, 0, 255), cv2.FILLED)
+            length = math.hypot(x2 - x1, y2 - y1)
+
+        return length, img, [x1, y1, x2, y2, cx, cy]
+
+
+def main():
+    pTime = 0
+    cTime = 0
+    cap = cv2.VideoCapture(1)
+    detector = handDetector()
+    while True:
+        success, img = cap.read()
+        img = detector.findHands(img)        # 检测手势并画上骨架信息
+
+        lmList = detector.findPosition(img)  # 获取得到坐标点的列表
+        if len(lmList) != 0:
+            print(lmList[4])
+
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+
+        cv2.putText(img, 'fps:' + str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+        cv2.imshow('Image', img)
+        cv2.waitKey(1)
+
+
+if __name__ == "__main__":
+    main()
