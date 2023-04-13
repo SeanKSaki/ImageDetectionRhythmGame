3d8502cc870c39187f9b277ff49a8bb5433b0d17
Eric Wu <ericwu2266@gmail.com>
2023-04-12 23:56:23 -0700

    Alpha game

--- AiVirtualMouse/main.py
+++ AiVirtualMouse/main.py
@@ -6,18 +6,6 @@ import keyboard
 import tkinter
 from cvzone.HandTrackingModule import HandDetector
 
-wCam, hCam = 1280, 720
-frameR = 144
-smoothening = 1
-target_radius = 40
-hand_radius = 40  
-border_size = 15 
-
-cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, wCam) 
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, hCam)  
-detector = HandDetector(detectionCon=0.8, maxHands=2)
-
 class Circle:
 
     def __init__(self, coordinates, radius, color, thickness):
@@ -39,57 +27,27 @@ class Circle:
             return False
 
 
-def hit_detection():
-    if keyboard.is_pressed('d') and state == 0:
-        return True
-    elif keyboard.is_pressed('f') and state == 1:
-        return True
-    elif keyboard.is_pressed('j') and state == 2:
-        return True
-    elif keyboard.is_pressed('k') and state == 3:
-        return True
-    else:
-        return False
-
-def create_random_target(current_target_pos=[]):
-    if current_target_pos:
-        possible_x = []
-
-        x_limit = [target_radius + border_size + 15, wCam - target_radius - border_size - 15]
-        y_limit = [target_radius + border_size + 15, hCam - target_radius - border_size - 15]
-
-        for i in range(x_limit[0], x_limit[1]):
-            if i + 100 < current_target_pos[0] or i - 100 > current_target_pos[0]:
-                possible_x.append(i)
-
-        possible_y = []
-        for i in range(y_limit[0], y_limit[1]):
-            if i + 100 < current_target_pos[1] or i - 100 > current_target_pos[1]:
-                possible_y.append(i)
-
-        if not possible_x:
-            possible_x = range(x_limit[0], x_limit[1])
-
-        if not possible_y:
-            possible_y = range(y_limit[0], y_limit[1])
-
-    else:
-        possible_x = range(target_radius + border_size, wCam - target_radius - border_size)
-        possible_y = range(target_radius + border_size, hCam - target_radius - border_size)
-
-    random_x = random.choice(possible_x)
-    random_y = random.choice(possible_y)
-    random_color = [random.randint(0, 255), random.randint(0, 255), random.randint(0, 256)]
-    _target = Circle([random_x, random_y], target_radius, random_color, -1)
-
-    return _target
+def hit_detection(_target):
+    if c.coords(_target)[0] > 40 and c.coords(_target)[0] < 160:
+        if keyboard.is_pressed('d') and state == 0:
+            return True
+        elif keyboard.is_pressed('f') and state == 1:
+            return True
+        elif keyboard.is_pressed('j') and state == 2:
+            return True
+        elif keyboard.is_pressed('k') and state == 3:
+            return True
+        else:
+            return False
 
 def generate_target(_canvas, _window):
     r = 60
     width = _window.winfo_screenwidth()
     height = _window.winfo_screenheight()
-    x = random.randint(0, width)
-    y = random.randint(0, height)
+    #x = random.randint(0, width)
+    x = width
+    #y = random.randint(0, height)
+    y = 540
     return _canvas.create_oval(x-r, y-r, x+r, y+r)
 
 #target = create_random_target()
@@ -104,27 +62,45 @@ c.pack(fill=tkinter.BOTH, expand=True)
 first = True
 state = 0;
 colors = ['red', 'blue', 'green', 'yellow']
+c.create_line(100,0,100,1080, fill="green", width=5)
+c.create_line(220,0,220,1080, fill="green", width=5)
+Text1 = c.create_text(960, 200, text = 'COMBO: 0', fill="black", font=('Helvetica 15 bold'))
+combo = 0
 while True:
     hit_target = False
+    miss_target = False
+
 
     r = 60
     width = window.winfo_screenwidth()
     height = window.winfo_screenheight()
-    x = random.randrange(0, width)
-    y = random.randrange(0, height)
+    #x = random.randrange(0, width)
+    x =  width
+    #y = random.randrange(0, height)
+    y = 540
     if first:
         target = c.create_oval(x-r, y-r, x+r, y+r, fill='red')
         first = False
 
-
-
     window.update_idletasks()
     window.update()
 
-    if hit_detection():
+    c.move(target, -0.2, 0)
+    print(c.coords(target))
+    if c.coords(target)[0] <= 0:
+        c.delete(Text1)
+        combo = 0
+        Text1 = c.create_text(960, 200, text = 'MISS!!!!!!', fill="black", font=('Helvetica 15 bold'))
+        miss_target = True
+
+    if hit_detection(target):
+        c.delete(Text1)
+        combo = combo + 1
+        string1 = 'COMBO: ' + str(combo)
+        Text1 = c.create_text(960, 200, text = string1, fill="black", font=('Helvetica 15 bold'))
         hit_target = True
 
-    if hit_target:
+    if hit_target or miss_target:
         c.delete(target)
         temp = random.randrange(0, 4)
         if temp == state:
