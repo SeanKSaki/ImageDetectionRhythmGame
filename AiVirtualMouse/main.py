5ce6a225a880bcefe4062cb7c1869431f574973a
Leonel Paredes Barrera <61176170+leonelpar00@users.noreply.github.com>
2023-04-25 20:30:23 -0700

    Add files via upload

--- AiVirtualMouse/main.py
+++ AiVirtualMouse/main.py
@@ -1,142 +1,127 @@
-import cv2
-import math
+import os 
 import random
-import time
-import mediapipe as mp
-from cvzone.HandTrackingModule import HandDetector
+import keyboard
+import tkinter
+from time import perf_counter
 
-wCam, hCam = 1280, 720
-frameR = 144
-smoothening = 1
-target_radius = 40
-hand_radius = 40  
-border_size = 15 
+beatmap_path = 'audio/music.beatmap.txt'
 
-cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, wCam) 
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, hCam)  
-detector = HandDetector(detectionCon=0.7, maxHands=2)
-mpHands = mp.solutions.hands
-hands = mpHands.Hands()
-mpDraw = mp.solutions.drawing_utils
+def read_beatmap_file(beatmap_path):
+    beatmap_path_full = os.path.join(beatmap_path)
+    with open(beatmap_path_full, 'rt') as f:
+        text = f.read()
+    onset_times = [float(string) for string in text.split('\n') if string != '']
+    return onset_times
 
-class Circle:
-
-    def __init__(self, coordinates, radius, color, thickness):
-        self.coordinates = coordinates
-        self.radius = radius
-        self.color = color
-        self.thickness = thickness
-
-    def draw(self, _frame):
-        cv2.circle(_frame, self.coordinates, self.radius, self.color, self.thickness)
-
-    def check_intersection(self, other_coordinates, other_radius):
-        distance = math.sqrt(math.pow(other_coordinates[0] - self.coordinates[0], 2) + math.pow(
-            other_coordinates[1] - self.coordinates[1], 2))
-
-        if distance <= self.radius + other_radius:
+def hit_detection(_target):
+    if c.coords(_target)[0] > 40 and c.coords(_target)[0] < 160:
+        if keyboard.is_pressed('d') and state == 0:
+            return True
+        elif keyboard.is_pressed('f') and state == 1:
+            return True
+        elif keyboard.is_pressed('j') and state == 2:
+            return True
+        elif keyboard.is_pressed('k') and state == 3:
             return True
         else:
             return False
+        
+def generate_target(_canvas, _window):
+    r = 60
+    width = _window.winfo_screenwidth()
+    height = _window.winfo_screenheight()
+    #x = random.randint(0, width)
+    x = width
+    #y = random.randint(0, height)
+    y = 540
+    return _canvas.create_oval(x-r, y-r, x+r, y+r)
 
+onset_times = read_beatmap_file(beatmap_path)
+num_notes = len(onset_times)
+random_track_indices = [
+        random.randint(0, 3) for _ in range(num_notes)
+        ]
+active_notes_per_track = {
+            track_idx: [] for track_idx in range(3)
+            }
+onset_hits = {
+            onset: False for onset in onset_times
+            }
 
-def create_random_target(current_target_pos=[]):
-    if current_target_pos:
-        possible_x = []
-
-        x_limit = [target_radius + border_size + 15, wCam - target_radius - border_size - 15]
-        y_limit = [target_radius + border_size + 15, hCam - target_radius - border_size - 15]
-
-        for i in range(x_limit[0], x_limit[1]):
-            if i + 100 < current_target_pos[0] or i - 100 > current_target_pos[0]:
-                possible_x.append(i)
-
-        possible_y = []
-        for i in range(y_limit[0], y_limit[1]):
-            if i + 100 < current_target_pos[1] or i - 100 > current_target_pos[1]:
-                possible_y.append(i)
-
-        if not possible_x:
-            possible_x = range(x_limit[0], x_limit[1])
-
-        if not possible_y:
-            possible_y = range(y_limit[0], y_limit[1])
-
-    else:
-        possible_x = range(target_radius + border_size, wCam - target_radius - border_size)
-        possible_y = range(target_radius + border_size, hCam - target_radius - border_size)
-
-    random_x = random.choice(possible_x)
-    random_y = random.choice(possible_y)
-    random_color = [random.randint(0, 255), random.randint(0, 255), random.randint(0, 256)]
-    _target = Circle([random_x, random_y], target_radius, random_color, -1)
-
-    return _target
-
-
-target = create_random_target()
-
-while True:
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    success, img = cap.read()
-    handsFrame = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    results = hands.process(handsFrame)
-    img = cv2.flip(img, 1)
-    img = cv2.copyMakeBorder(img, border_size, border_size, border_size, border_size, cv2.BORDER_CONSTANT,
-                               value=[0, 0, 0])
-
-    hit_target = False
-    hand_detect = detector.findHands(img, flipType=False, draw=False)
-    target.draw(img)
-    fist = False
+is_playing = False
+window = tkinter.Tk()
+window.title("Game Screen")
+window.attributes('-fullscreen', True)
+c = tkinter.Canvas(window, bg='white', highlightthickness=0)
+c.pack(fill=tkinter.BOTH, expand=True)
+#target = generate_target(c, window)
+first = True
+state = 0;
+colors = ['red', 'blue', 'green', 'yellow']
+c.create_line(100,0,100,1080, fill="green", width=5)
+c.create_line(220,0,220,1080, fill="green", width=5)
+c.create_line(0, 300, 1920, 300, fill="black", width = 5)
+c.create_line(0, 420, 1920, 420, fill="black", width = 5)
+c.create_line(0, 540, 1920, 540, fill="black", width = 5)
+c.create_line(0, 660, 1920, 660, fill="black", width = 5)
+c.create_line(0, 780, 1920, 780, fill="black", width = 5)
     
-    if results.multi_hand_landmarks:
-                for handLMS in results.multi_hand_landmarks:
-                    lmList = []
-                    for id, lm in enumerate(handLMS.landmark):
-                        h, w, c = handsFrame.shape
-                        cx, cy = int(lm.x * w), int(lm.y * h)
-                        lmList.append([id, cx, cy])
-                    indexX = 0
-                    indexY = 0
-                    indexMid = 0
-                    handBottomX = 0
-                    handBottomY = 0
-                    pinkyX = 0
-                    pinkyY = 0
-                    fistWarning = "Fist!"
-                    for lms in lmList:
-                        if lms[0] == 7:
-                            indexX, indexY = lms[1], lms[2]
-                        elif lms[0] == 5:
-                            indexMid = lms[2]
-                        elif lms[0] == 19:
-                            pinkyX, pinkyY = lms[1], lms[2]
-                        elif lms[0] == 0:
-                            handBottomX, handBottomY = lms[1], lms[2]
-                    if (indexY < handBottomY) and (indexY > indexMid):
-                        cv2.rectangle(handsFrame, (indexX, indexY), (pinkyX, handBottomY), (0, 0, 255), 2)
-                        cv2.putText(handsFrame, fistWarning, (pinkyX + 2, indexY - 2), (font), .7,
-                                    (0, 0, 255), 1, cv2.LINE_4)
-                        fist = True
-                        
-    if hand_detect:
-        for i in range(len(hand_detect)):
-            hand_position = hand_detect[i]["center"]
-            hand_circle = Circle(hand_position, hand_radius, (0, 0, 255), 1)
+Text1 = c.create_text(960, 200, text = 'COMBO: 0', fill="black", font=('Helvetica 15 bold'))
+combo = 0
+i = 0
+while True:
+        
+    hit_target = False
+    miss_target = False
+    timer = perf_counter()
+    current_time = "%.4f" % timer
+    #print(current_time)
 
-            if target.check_intersection(hand_circle.coordinates, hand_circle.radius):
-                hand_circle.color = (0,255,0)
-                hit_target = True
-            else:
-                hand_circle.color = (0,0,255)
-            hand_circle.draw(img)
-            
-    if hit_target and fist:
-        target = create_random_target(target.coordinates)
+    r = 60
+    width = window.winfo_screenwidth()
+    height = window.winfo_screenheight()
+    #x = random.randrange(0, width)
+    x =  width
+    #y = random.randrange(0, height)
+    y = 540
+    if first:
+        target = c.create_oval(x-r, y-r+(3*r), x+r, y+r+(3*r), fill='red')
+        first = False
 
-    cv2.namedWindow("Image", cv2.WND_PROP_FULLSCREEN)
-    cv2.setWindowProperty("Image", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-    cv2.imshow("Image", img)
-    cv2.waitKey(1)
+    window.update_idletasks()
+    window.update()
+    
+
+    c.move(target, -0.4, 0)
+    #print(c.coords(target))
+    if c.coords(target)[0] <= 0:
+        c.delete(Text1)
+        combo = 0
+        Text1 = c.create_text(960, 200, text = 'MISS!!!!!!', fill="black", font=('Helvetica 15 bold'))
+        miss_target = True
+
+    if hit_detection(target):
+        c.delete(Text1)
+        combo = combo + 1
+        string1 = 'COMBO: ' + str(combo)
+        Text1 = c.create_text(960, 200, text = string1, fill="black", font=('Helvetica 15 bold'))
+        hit_target = True
+
+        
+    if hit_target or miss_target:
+        c.delete(target) 
+        state = random_track_indices[i] 
+        i = i + 1
+        if state == 0:
+            target = c.create_oval(x-r, y-r+(3*r), x+r, y+r+(3*r), fill=colors[state])
+        elif state == 1:
+            target = c.create_oval(x-r, y-r+(1*r), x+r, y+r+(1*r), fill=colors[state])
+        elif state == 2:
+            target = c.create_oval(x-r, y-r+(-1*r), x+r, y+r+(-1*r), fill=colors[state])
+        elif state == 3:
+            target = c.create_oval(x-r, y-r+(-3*r), x+r, y+r+(-3*r), fill=colors[state])
+    
+    if keyboard.is_pressed('esc'):
+        print(onset_times)
+        print(random_track_indices)
+        break
